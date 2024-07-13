d70abf800eb5a9d365a97acf4de3c15a6f6353a6
aneylon <arlen.m.neylon@gmail.com>
2024-07-12 18:35:59 -0600

    Add more list info.

--- PythonCrashCourse/03/motorcycles.py
+++ PythonCrashCourse/03/motorcycles.py
@@ -3,3 +3,23 @@ print(motorcycles)
 
 motorcycles[0] = 'ducati'
 print(motorcycles)
+
+motorcycles.append('honda')
+print(motorcycles)
+
+motorcycles.insert(0, 'arlen ness')
+print(motorcycles)
+
+del motorcycles[1]
+print(motorcycles)
+
+popped = motorcycles.pop()
+print(popped)
+print(motorcycles)
+
+first = motorcycles.pop(0)
+print(f'The first item was {first.title()}')
+
+removed = motorcycles.remove('yamaha')
+print(removed)
+print(motorcycles)
