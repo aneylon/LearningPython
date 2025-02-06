17a9b77025c3511a32398ec9e4c27b1c3ba1d8fe
aneylon <arlen.m.neylon@gmail.com>
2025-02-05 19:26:05 -0700

    Add first chapter four exercises.

---
+++ PythonCrashCourse/04/animals.py
@@ -0,0 +1,6 @@
+animals = ['dog', 'cat', 'snake', 'spider']
+for animal in animals:
+    # print(animal)
+    print(f"A {animal} would make a great pet!")
+
+print(f"These are all great pets!")
---
+++ PythonCrashCourse/04/pizzas.py
@@ -0,0 +1,6 @@
+pizzas = ['cheese', 'peperoni', 'sausage']
+for pizza in pizzas:
+    # print(pizza)
+    print(f"{pizza.title()}! Order up!")
+
+print('All the pizzas are out!')
