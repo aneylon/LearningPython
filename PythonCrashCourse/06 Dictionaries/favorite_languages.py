a09e193ea500331ad98a7d4d60fd6b59d978f5af
aneylon <arlen.m.neylon@gmail.com>
2026-07-27 10:41:57 -0600

    Add dictionary example.

---
+++ PythonCrashCourse/06 Dictionaries/favorite_languages.py
@@ -0,0 +1,9 @@
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'rust',
+    'phil': 'python'
+}
+
+for lang in favorite_languages :
+    print ( f"{lang}'s favorite language is {favorite_languages[lang]}" )
