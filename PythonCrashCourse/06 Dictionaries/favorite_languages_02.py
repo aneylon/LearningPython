86bcc0dac98e1e211f4def9dad2a3bbc24cdd551
aneylon <arlen.m.neylon@gmail.com>
2026-07-30 08:13:54 -0600

    Add dictionary example.

---
+++ PythonCrashCourse/06 Dictionaries/favorite_languages_02.py
@@ -0,0 +1,17 @@
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'rust',
+    'phil' : 'python'
+}
+
+for name, language in favorite_languages.items() :
+    print ( f"{name.title()}'s favorite language is {language.title()}")
+
+friends = ['phil', 'sarah']
+for name in favorite_languages.keys() :
+    print (f"Hi {name.title()}")
+
+    if name in friends :
+        language = favorite_languages[name].title()
+        print ( f"\t {name.title()}, I see you love {language}")
