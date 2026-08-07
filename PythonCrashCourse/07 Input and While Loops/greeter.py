6d45e6dfe0073339d34f5204344571b2567e058b
aneylon <arlen.m.neylon@gmail.com>
2026-08-07 14:28:41 -0600

    Add input example.

--- PythonCrashCourse/07 Input and While Loops/greeter.py
+++ PythonCrashCourse/07 Input and While Loops/greeter.py
@@ -1,3 +1,11 @@
 name = input( "What is your name ? ")
 
 print ( f"What is your favorite color {name}?")
+
+age = input( "How old are you? " )
+age = int(age)
+
+if age >= 18 :
+    print ("Old enough to buy cigs.")
+
+
