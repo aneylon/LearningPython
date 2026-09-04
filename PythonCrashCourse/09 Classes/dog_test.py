e6d53135030f34741b918308f0663c89dc5e2ff8
aneylon <arlen.m.neylon@gmail.com>
2026-09-04 10:23:09 -0600

    Add class examples.

--- PythonCrashCourse/09 Classes/dog_test.py
+++ PythonCrashCourse/09 Classes/dog_test.py
@@ -1,8 +1,15 @@
 import dog
 my_dog = dog.Dog('Willie', 6)
+your_dog = dog.Dog('Lucy', 3)
 
 print(f"My dog's name is {my_dog.name}")
 print(f"My dog is {my_dog.age} years old.")
 
 my_dog.sit()
 my_dog.roll_over()
+
+print(f"Your dog's name is {your_dog.name}")
+print(f"Your dog is {your_dog.age} years old.")
+
+your_dog.sit()
+your_dog.roll_over()
