b6aabb22955ea31cfacb544bb977f4e7ef60ab09
aneylon <arlen.m.neylon@gmail.com>
2026-09-06 19:03:53 -0600

    Add class example.'

--- PythonCrashCourse/09 Classes/car.py
+++ PythonCrashCourse/09 Classes/car.py
@@ -3,6 +3,10 @@ class Car :
         self.make = make
         self.model = model
         self.year = year
+        self.odometer_reading = 0
+
+    def read_odometer(self) : 
+        print(f"This car has {self.odometer_reading} miles on it")
 
     def get_descriptive_name(self):
         long_name = f"{self.year} {self.make} {self.model}"
@@ -10,4 +14,4 @@ class Car :
 
 my_new_car = Car("audi", "a4", 2024)
 print(my_new_car.get_descriptive_name())
-
+my_new_car.read_odometer()
