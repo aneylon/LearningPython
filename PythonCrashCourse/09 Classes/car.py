18f870f3ef161b6bb18d0bbed802e666a8a48a5c
aneylon <arlen.m.neylon@gmail.com>
2026-09-10 07:27:16 -0600

    Add class examples.

--- PythonCrashCourse/09 Classes/car.py
+++ PythonCrashCourse/09 Classes/car.py
@@ -24,9 +24,14 @@ class Car :
 class ElectricCar(Car):
     def __init__(self, make, model, year):
         super().__init__(make, model, year)
+        self.battery_size = 40
+
+    def describe_battery(self):
+        print(f"This car has a {self.battery_size}-kWh battery.")
 
 my_leaf = ElectricCar('nissan', 'leaf', 2024)
 print(my_leaf.get_descriptive_name())
+my_leaf.describe_battery()
 
 my_new_car = Car("audi", "a4", 2024)
 print(my_new_car.get_descriptive_name())
