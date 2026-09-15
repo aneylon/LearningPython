edb28d8f3a512bd9844d4c5b618fee0b454b9a83
aneylon <arlen.m.neylon@gmail.com>
2026-09-14 19:57:49 -0600

    Add class example.

--- PythonCrashCourse/09 Classes/car.py
+++ PythonCrashCourse/09 Classes/car.py
@@ -29,9 +29,13 @@ class ElectricCar(Car):
     def describe_battery(self):
         print(f"This car has a {self.battery_size}-kWh battery.")
 
+    def fill_gas_tank(self) :
+        print("This car doesn't have a gas tank!")
+
 my_leaf = ElectricCar('nissan', 'leaf', 2024)
 print(my_leaf.get_descriptive_name())
 my_leaf.describe_battery()
+my_leaf.fill_gas_tank()
 
 my_new_car = Car("audi", "a4", 2024)
 print(my_new_car.get_descriptive_name())
