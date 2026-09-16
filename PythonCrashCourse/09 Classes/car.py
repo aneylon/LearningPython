e761e8cc49c54d448861318acf4b62ed0dd8e1b5
aneylon <arlen.m.neylon@gmail.com>
2026-09-16 10:35:49 -0600

    Add class example.

--- PythonCrashCourse/09 Classes/car.py
+++ PythonCrashCourse/09 Classes/car.py
@@ -26,6 +26,16 @@ class Battery:
 
     def describe_battery(self):
         print(f"This car has a {self.battery_size}-kWh battery.")
+
+    def get_range(self) :
+        if(self.battery_size == 40) :
+            range = 150
+        elif(self.battery_size == 65) :
+            range = 225
+
+        print(f"This car can go about { range } miles on a full charge.")
+
+
         
 class ElectricCar(Car):
     def __init__(self, make, model, year):
@@ -44,6 +54,7 @@ print(my_leaf.get_descriptive_name())
 # my_leaf.describe_battery()
 my_leaf.battery.describe_battery()
 my_leaf.fill_gas_tank()
+my_leaf.battery.get_range()
 
 my_new_car = Car("audi", "a4", 2024)
 print(my_new_car.get_descriptive_name())
