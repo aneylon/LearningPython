940c60f0b1ea65c8225ccece5cb0b36c050aa407
aneylon <arlen.m.neylon@gmail.com>
2026-09-17 08:26:23 -0600

    Add class example.

--- PythonCrashCourse/09 Classes/car.py
+++ PythonCrashCourse/09 Classes/car.py
@@ -49,25 +49,25 @@ class ElectricCar(Car):
     def fill_gas_tank(self) :
         print("This car doesn't have a gas tank!")
 
-my_leaf = ElectricCar('nissan', 'leaf', 2024)
-print(my_leaf.get_descriptive_name())
-# my_leaf.describe_battery()
-my_leaf.battery.describe_battery()
-my_leaf.fill_gas_tank()
-my_leaf.battery.get_range()
+# my_leaf = ElectricCar('nissan', 'leaf', 2024)
+# print(my_leaf.get_descriptive_name())
+# # my_leaf.describe_battery()
+# my_leaf.battery.describe_battery()
+# my_leaf.fill_gas_tank()
+# my_leaf.battery.get_range()
 
-my_new_car = Car("audi", "a4", 2024)
-print(my_new_car.get_descriptive_name())
-my_new_car.read_odometer()
+# my_new_car = Car("audi", "a4", 2024)
+# print(my_new_car.get_descriptive_name())
+# my_new_car.read_odometer()
 
-my_new_car.update_odometer(23)
+# my_new_car.update_odometer(23)
 
-my_new_car.read_odometer()
+# my_new_car.read_odometer()
 
-my_used_car = Car("subaru", "outback", 2019)
+# my_used_car = Car("subaru", "outback", 2019)
 
-my_used_car.update_odometer(23_500)
-my_used_car.read_odometer()
+# my_used_car.update_odometer(23_500)
+# my_used_car.read_odometer()
 
-my_used_car.increment_odometer(100)
-my_used_car.read_odometer()
+# my_used_car.increment_odometer(100)
+# my_used_car.read_odometer()
