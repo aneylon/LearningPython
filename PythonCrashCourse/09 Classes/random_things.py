f30c57d47dcbfe646903e9a7c6790282a53963b8
aneylon <arlen.m.neylon@gmail.com>
2026-09-23 09:59:12 -0600

    Add examples with random.

---
+++ PythonCrashCourse/09 Classes/random_things.py
@@ -0,0 +1,11 @@
+from random import randint
+from random import choice
+
+something = randint(1,6)
+
+print(something)
+
+players = ['one', 'two', 'three', 'four']
+first_up = choice(players)
+
+print(first_up)
